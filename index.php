900d389a30ec9c6fe36c7590cabb3a5687972251
p4tz111 <p4t.s4m.l1@gmail.com>
2026-05-12 08:35:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = "Startseite";
+include 'header.php';
+?>
+
+<main>
+    <h1>Willkommen</h1>
+    <p>Das ist der Inhalt der Seite.</p>
+</main>
+
+<?php
+include 'footer.php';
+?>
